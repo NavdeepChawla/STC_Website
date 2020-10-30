690ef81b18a1027193361d47afac2752598f47de
navdeepchawla <navdeepchawla2000@gmail.com>
2020-10-31 00:40:20 +0530

    Initial Commit

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("SideNav").style.width = "50vw";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("SideNav").style.width = "0";
+  }
